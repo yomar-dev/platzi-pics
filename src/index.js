b3cce4ea1a438fc89d5b2e965c4e528b58cb646b
Yomar Miranda <mirandayomar@gmail.com>
2018-05-11 23:29:51 -0500

    Importar nuestro 'devtools' en nuestro archivo principal.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
  * visual de la aplicación de escritorio.
  */
 import { app, BrowserWindow } from 'electron'
-import devtools from './devtools.js'
+import devtools from './devtools'
 
 /**
  * 'before-quit': Es un evento que se ejecuta antes de cerrar la aplicación.
