152c7e9785e7b9ec4c470668694fab86f9e56d16
Yomar Miranda <mirandayomar@gmail.com>
2018-05-20 22:50:25 -0500

    Buscar imagenes.

--- src/renderer/frontend.js
+++ src/renderer/frontend.js
@@ -1,5 +1,9 @@
+import url from 'url'
+import path from 'path'
+
 window.addEventListener('load', () => {
 	addImagesEvents();
+	searchImagesEvent();
 });
 
 function addImagesEvents(){
@@ -16,4 +20,25 @@ function changeImage(node){
 	document.querySelector('li.selected').classList.remove('selected');
 	node.classList.add('selected');
 	document.getElementById('image-displayed').src = node.querySelector('img').src;
+}
+
+function searchImagesEvent(){
+	const searchBox = document.getElementById('search-box');
+
+	searchBox.addEventListener('keyup', function(){
+		const regex = new RegExp(this.value.toLowerCase(), 'gi');
+		if(this.value.length > 0){
+			const thumbs = document.querySelectorAll('li.list-group-item img');
+			const lengthThumbs = thumbs.length;
+			for (var i = 0; i < lengthThumbs; i++) {
+				const fileUrl = url.parse(thumbs[i].src);
+				const fileName = path.basename(fileUrl.pathname);
+				if(fileName.match(regex)){
+					thumbs[i].parentNode.classList.remove('hidden');
+				}else{
+					thumbs[i].parentNode.classList.add('hidden');					
+				}
+			}
+		}
+	})
 }
