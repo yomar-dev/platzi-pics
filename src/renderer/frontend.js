5b4370fcbdafee1df4d436484b9df426a8c2da7e
Yomar Miranda <mirandayomar@gmail.com>
2018-05-20 20:54:29 -0500

    Seleccionar imagenes.

--- src/renderer/frontend.js
+++ src/renderer/frontend.js
@@ -1,5 +1,19 @@
-import os from 'os'
-
 window.addEventListener('load', () => {
-	console.log(os.cpus());
+	addImagesEvents();
 });
+
+function addImagesEvents(){
+	const thumbs = document.querySelectorAll('li.list-group-item');
+	const lengthThumbs = thumbs.length;
+	for (var i = 0; i < lengthThumbs; i++) {
+		thumbs[i].addEventListener('click', function (){
+			changeImage(this);
+		});
+	}
+}
+
+function changeImage(node){
+	document.querySelector('li.selected').classList.remove('selected');
+	node.classList.add('selected');
+	document.getElementById('image-displayed').src = node.querySelector('img').src;
+}
