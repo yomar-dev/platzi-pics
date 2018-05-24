92d207cd95faba62b80715009a6e633bd3f66e44
Yomar Miranda <mirandayomar@gmail.com>
2018-05-24 00:00:01 -0500

    Mostrar las imagenes seleccionadas en el panel izquierdo de la aplicación.

--- src/renderer/IpcRendererEvents.js
+++ src/renderer/IpcRendererEvents.js
@@ -7,7 +7,7 @@ import { ipcRenderer } from 'electron'
 function setIpc(){
 	ipcRenderer.on('load-images', (event, images) => {
 		clearImages();
-		console.log(images);
+		loadImages(images);
 	});
 }
 
@@ -23,9 +23,25 @@ function openDirectory(){
  */
 function clearImages(){
 	const oldImages = document.querySelectorAll('li.list-group-item');
-	let totalRecords = oldImages.length;
-	for (var i = 0; i < totalRecords; i++) {
+	const totalRecords = oldImages.length;
+	for (let i = 0; i < totalRecords; i++) {
 		oldImages[i].parentNode.removeChild(oldImages[i]);
+	}
+}
+
+function loadImages(images){
+	let imagesList = document.querySelector('ul.list-group')
+	const totalRecords = images.length;
+	for (let i = 0; i < totalRecords; i++) {
+		
+		const node = `<li class="list-group-item">
+				    	<img class="media-object pull-left" src="${images[i].src}" height="32">
+				    	<div class="media-body">
+				      		<strong>${images[i].filename}</strong>
+				      		<p>${images[i].size}</p>
+				    	</div>
+				  	</li>`;
+		imagesList.insertAdjacentHTML('beforeend', node);
 	}
 }
 
